97b8b55a059463ef1fcd2ed16ac2add96f66ecf0
Aarya Linesh Mody <aaryamody0418@gmail.com>
2026-09-03 12:23:07 +0530

    Test canonical training view adapter

--- tests/test_ui_adapters.py
+++ tests/test_ui_adapters.py
@@ -5,10 +5,13 @@ import pandas as pd
 
 from src.core.database.compat_exports import export_legacy_student_csvs
 from src.core.database.service import DatabaseService
+from src.core.face_engine import FaceEngine
+from src.ui import legacy_training_view
 from src.ui.attendance_view import AttendanceView
 from src.ui.legacy_attendance_view import AttendanceView as LegacyAttendanceView
 from src.ui.legacy_student_registration import StudentRegistrationView as LegacyRegistrationView
 from src.ui.student_registration import StudentRegistrationView
+from src.ui.training_view import TrainingView
 
 
 def test_public_attendance_view_overrides_legacy_persistence():
@@ -21,6 +24,12 @@ def test_public_attendance_view_overrides_legacy_persistence():
 def test_public_registration_view_overrides_legacy_persistence():
     assert issubclass(StudentRegistrationView, LegacyRegistrationView)
     assert StudentRegistrationView.save_student_details is not LegacyRegistrationView.save_student_details
+
+
+def test_public_training_view_uses_canonical_face_and_student_services():
+    assert issubclass(TrainingView, legacy_training_view.TrainingView)
+    assert TrainingView._update_student_info is not legacy_training_view.TrainingView._update_student_info
+    assert legacy_training_view.FaceDetector is FaceEngine
 
 
 def test_legacy_student_csvs_are_derived_from_sqlite(tmp_path, monkeypatch):
